b797f37a93bfce2e2b3634814100a1fdb2a11865
Maddi Chaitanya Sai <maddichaitanyasai@gmail.com>
2026-08-06 10:48:56 +0530

    Remove Facebook login option

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -85,8 +85,8 @@ export const Login = () => {
             </div>
           )}
 
-          {/* Social Logins (Google & Facebook) */}
-          <div className="space-y-2.5">
+          {/* Social Login (Google) */}
+          <div>
             <button
               type="button"
               onClick={() => handleSocialLogin('google')}
@@ -101,19 +101,6 @@ export const Login = () => {
                 <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.06l3.66 2.84c.87-2.6 3.3-4.52 6.16-4.52z" />
               </svg>
               <span>Continue with Google</span>
-            </button>
-
-            <button
-              type="button"
-              onClick={() => handleSocialLogin('facebook')}
-              disabled={loading}
-              className="w-full py-2.5 px-4 rounded-xl border border-blue-200 bg-[#1877F2] hover:bg-[#166fe5] text-white text-xs font-bold transition-all flex items-center justify-center space-x-2.5 shadow-2xs cursor-pointer"
-            >
-              {/* Facebook SVG Logo */}
-              <svg className="w-4 h-4 fill-current text-white" viewBox="0 0 24 24">
-                <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
-              </svg>
-              <span>Continue with Facebook</span>
             </button>
           </div>
 
